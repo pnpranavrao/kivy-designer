4e5c56817537a0bded7d7bdab311c4d769594ee9
Pranav Rao <pnpranavrao@gmail.com>
2012-05-29 14:21:54 +0100

    Add status bar. Filter widget menu to include only widgets. Squash some scroll bugs

--- designer.py
+++ designer.py
@@ -3,6 +3,7 @@ kivy.require('1.0.9')
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.layout import Layout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics import Color, Rectangle, PushMatrix, PopMatrix, \
@@ -19,41 +20,52 @@ from kivy.uix.treeview import TreeView
 from kivy.uix.treeview import TreeViewLabel
 from kivy.factory import Factory
 from kivy.vector import Vector
-import pkgutil
+from kivy.clock import Clock
 import random
+from functools import partial
 
 Builder.load_string('''#:kivy 1.0.9
 <designer>:
+    status_bar:status_bar
     canvas_area:canvas_area
     treeview:treeview
-    #prog_bar:prog_bar
+    win:win
     #widget_box:widget_box
     BoxLayout:
         id:win
         orientation:'horizontal'
-        #Canvas box
-        FloatLayout:
-            id:canvas_area
-            size_hint:.8,1
-            canvas:
-                Color:
-                    rgb:1,.9,.8
-                Rectangle:
-                    pos: self.x, self.y
-                    size: self.width,self.top
+        BoxLayout:
+            size_hint:.7,1
+            orientation:'vertical'
+            #Canvas box
+            FloatLayout:
+                size_hint:1,.95
+                id:canvas_area
+                canvas:
+                    Color:
+                        rgb:1,.9,.8
+                    Rectangle:
+                        pos: self.x, self.y
+                        size: self.width,self.top
+                Label:
+                    color:0,0,0,1
+                    text:'Canvas Area'
+                    text_size:(300,300)
+                    pos:150,200
+                    #Doubts :
+                    #size_hint:0.2,0.2 >>Also screws things up
+                    #pos_hint:(.5,.5) >> Why doesn't this work?
+                    #pos:root.width/2,root.top/2 >> how to reference FloatLayout?
+            #Statusbar
             Label:
-                color:0,0,0,1
-                text:'Canvas Area'
-                text_size:(300,300)
-                pos:150,200
-                #Doubts :
-                #size_hint:0.2,0.2 >>Also screws things up
-                #pos_hint:(.5,.5) >> Why doesn't this work?
-                #pos:root.width/2,root.top/2 >> how to reference FloatLayout?
-                
+                markup:True
+                id:status_bar
+                text:self.text if self.text else ""
+                size_hint:1,.05
+                    
         #widgets box
         BoxLayout:
-            size_hint:.4,1
+            size_hint:.3,1
             ScrollView:
                 TreeView:
                     id: treeview
@@ -83,30 +95,57 @@ class TreeViewProperty(BoxLayout,TreeViewNode):
     rkey = ObjectProperty(None)
     
 class designer(FloatLayout):
+    status_bar = ObjectProperty(None)
     canvas_area = ObjectProperty(None)
     treeview = ObjectProperty(None)
     win = ObjectProperty(None)
     saved_nodes = ObjectProperty(None)
     saved_nodes = []
-    
-    for entry in pkgutil.iter_modules(path=kivy.uix.__path__):
-        module_name = "kivy.uix."+entry[1]
-        class_name = entry[1].title()
-        Factory.register(class_name, module=module_name)
+    widget_list = {}
+    # get any widgets in kivy.uix directory
+    for cls in Factory.classes:
+        module_string = str(Factory.classes[cls]['module'])
+        # if module is from current projects uix directory(user defined widgets) or 'kivy.uix' (kivy defined widgets)
+        if module_string.startswith(('uix', 'kivy.uix')):
+            widget_list[cls]=str(Factory.classes[cls]['module'])
+            Factory.register(cls,module=widget_list[cls])
         
     def build(self):
         keys=[]
-        for i in pkgutil.iter_modules(path=kivy.uix.__path__):
-            keys.append(i[1]) 
+        for cls in self.widget_list:
+            if cls == "Camera":
+                '''This is because there seems to be some bug in Gstreamer 
+                when we load a camera widget and don't use it '''
+                continue
+            try:
+                factory_caller = getattr(Factory,str(cls))
+                new_widget = factory_caller()
+                if isinstance(new_widget,Layout):
+                    continue
+                if isinstance(new_widget,Widget):
+                    keys.append(cls)
+            except Exception as err:
+                self.print_status(err.message)
+        keys.append('Camera')
         keys.sort()
         node = None
         for key in keys:
             text = '%s' % key
-            text = text.title()
             node = TreeViewLabel(text=text)
             node.bind(is_selected = self.add_new_widget)
             self.treeview.add_node(node)
             self.saved_nodes.append(node)
+    
+    def print_status(self,msg):
+        """Provide a string as an argument to print it out in the status bar for 2 seconds"""
+        label = self.status_bar
+        label.text = "[b]Status Bar[/b] "+msg
+        g = lambda x,y:""
+        Clock.schedule_interval(self.clear_status, 2)
+        
+    def clear_status(self,*largs):
+        """Small function to clear the status bar after n seconds"""
+        self.status_bar.text = ""
         
     def add_new_widget(self, instance, value,index=-1, *l):
         if instance.is_selected:
@@ -117,7 +156,7 @@ class designer(FloatLayout):
             new_widget.bind(on_touch_up = self.show_properties)
             self.canvas_area.add_widget(new_widget)
             instance.is_selected = False
-    
+
     def drag(self,widget,touch):
         if widget.collide_point(touch.x,touch.y):
             widget.center = touch.pos
